77b04beaf4d469e45f54363b93c3135296ae2204
ZukisaNante <zukisanante@gmail.com>
2019-08-28 14:57:28 +0200

    -a

--- database.php
+++ database.php
@@ -11,11 +11,11 @@
 
     // Declare Variables
     define('DB_HOST', 'localhost');
-    define('DB_USER', '24843');
-    define('DB_PASS', 'zukisa31');
-    define('DB_NAME', '24843');
+    define('DB_USER', 'root');
+    define('DB_PASS', '');
+    define('DB_NAME', 'notes_db');
 
     // Database Connection
-    $connect = new PDO("mysql:host=localhost;dbname=notes_db", "root", "");
+    $connect = new PDO("mysql:host=localhost;dbname=24843", "24843", "zukisa31");
 
 ?>
