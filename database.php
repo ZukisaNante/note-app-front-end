cd32cecce5a79674e4e3bd27b3f208168a7930b2
ZukisaNante <zukisanante@gmail.com>
2019-08-28 14:50:42 +0200

    -a

--- database.php
+++ database.php
@@ -11,23 +11,11 @@
 
     // Declare Variables
     define('DB_HOST', 'localhost');
-    define('DB_USER', 'root');
-    define('DB_PASS', '');
-    define('DB_NAME', 'notes_db');
+    define('DB_USER', '24843');
+    define('DB_PASS', 'zukisa31');
+    define('DB_NAME', '24843');
 
     // Database Connection
     $connect = new PDO("mysql:host=localhost;dbname=notes_db", "root", "");
-
-
-    /* function connect()
-        {
-            $connect = mysqli_connect(DB_HOST ,DB_USER ,DB_PASS ,DB_NAME);
-            if (mysqli_connect_errno($connect)) {
-                die("Failed to connect:" . mysqli_connect_error());
-                }
-            mysqli_set_charset($connect, "utf8");
-        return $connect;
-        }
-$con = connect(); */
 
 ?>
